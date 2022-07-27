5b1ff42da8f0d54a5430e570c51b91b528a49292
t-kabu <t.kabu.03@gmail.com>
2022-07-28 00:04:40 +0900

    monaco editorとtailwind css導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
-import type { NextPage } from 'next'
+import Editor from '@monaco-editor/react'
+import React, { useState } from 'react'
 
-const Home: NextPage = () => {
-  return <h1>テスト</h1>
+export default function Home() {
+  const [text, setText] = useState<string | undefined>("const a = 'abc'")
+
+  return (
+    <div className="flex h-screen items-center justify-center  bg-gray-500">
+      <Editor
+        width="50vh"
+        height="70vh"
+        defaultValue={text || ''}
+        defaultLanguage="typescript"
+        onChange={setText}
+      />
+    </div>
+  )
 }
-
-export default Home
