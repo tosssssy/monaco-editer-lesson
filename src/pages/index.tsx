732214ac26206e3b36a3d67333a02b4eafc0d34a
t-kabu <t.kabu.03@gmail.com>
2022-07-28 22:31:15 +0900

    見た目を整える

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,32 @@
 import Editor from '@monaco-editor/react'
 import React, { useState } from 'react'
 
+const code =
+  ////////////////////////////////////////
+  `function sayHello(name: string) {
+  console.log('hello', name)
+}
+`
+
 export default function Home() {
-  const [text, setText] = useState<string | undefined>("const a = 'abc'")
+  const [text, setText] = useState<string | undefined>(code)
 
   return (
-    <div className="flex h-screen items-center justify-center  bg-gray-500">
-      <Editor
-        width="50vh"
-        height="70vh"
-        defaultValue={text || ''}
-        defaultLanguage="typescript"
-        onChange={setText}
-      />
+    <div className="h-screen bg-gray-200 pt-10">
+      <h1 className="text-center text-4xl">エディタサンプル</h1>
+      <div className="mt-24 flex justify-center gap-10">
+        <div className="w-[40%]">
+          <Editor
+            height="70vh"
+            defaultValue={text || ''}
+            defaultLanguage="typescript"
+            onChange={setText}
+          />
+        </div>
+        <div className="h-[70vh] w-[40%] bg-white">
+          <pre>{text}</pre>
+        </div>
+      </div>
     </div>
   )
 }
