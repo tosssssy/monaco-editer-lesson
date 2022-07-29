26cf28f2290737fa2efc2bc9d7d40fde4d8d32a5
t-kabu <t.kabu.03@gmail.com>
2022-07-29 18:44:46 +0900

    コードをリンクで共有できるように

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,42 @@
 import Editor from '@monaco-editor/react'
-import React, { useState } from 'react'
+import { Base64 } from 'js-base64'
+import { useRouter } from 'next/router'
+import React, { useEffect, useMemo, useState } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-const code =
+const BASE_URL = 'https://monaco-editor-lesson.netlify.app/'
+
+const defaultCode =
   ////////////////////////////////////////
   `function sayHello(name: string) {
-  console.log('hello', name)
+  console.log('hello' + name)
 }
 `
 
 export default function Home() {
-  const [text, setText] = useState<string | undefined>(code)
+  const router = useRouter()
+  const code = useMemo(
+    () => Base64.decode((router.query['code'] as string | undefined) || ''),
+    [router.query],
+  )
+  const [text, setText] = useState<string | undefined>(code || defaultCode)
+  const encoded = useMemo(() => Base64.encode(text || ''), [text])
+  useEffect(() => {
+    if (code) setText(code)
+  }, [code])
 
   return (
     <div className="h-screen bg-gray-200 pt-10">
       <h1 className="text-center text-4xl">エディタサンプル</h1>
-      <div className="mt-24 flex justify-center gap-10">
+      <CopyToClipboard
+        text={`${BASE_URL}?code=${encoded}`}
+        onCopy={() => alert('コピーしました')}
+      >
+        <button className="ml-40 block rounded-lg bg-sky-600 p-2 text-white hover:opacity-70">
+          共有リンクをコピー
+        </button>
+      </CopyToClipboard>
+      <div className="mt-10 flex justify-center gap-10">
         <div className="w-[40%]">
           <Editor
             height="70vh"
@@ -23,7 +45,7 @@ export default function Home() {
             onChange={setText}
           />
         </div>
-        <div className="h-[70vh] w-[40%] bg-white">
+        <div className="h-[70vh] w-[40%]  bg-white">
           <pre>{text}</pre>
         </div>
       </div>
